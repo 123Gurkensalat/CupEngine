09f8cde4656b07ec7778474b5c96437f8352877b
123Gurkensalat <florianzinck361@gmail.com>
2025-04-03 13:09:59 +0200

    changed: naming of event handlers

--- external/utils/event.h
+++ external/utils/event.h
@@ -7,22 +7,23 @@ namespace utils {
     template<typename ...Args>
     class Event {
     public:
+        using Handler = std::function<void(Args...)>;
         Event() {}
         Event(Event<Args...>&) = delete;
         void operator=(Event<Args...>&) = delete;
 
-        void operator+=(std::function<void(Args...)> func) {
-            listeners.emplace_back(func);
+        void operator+= (const Handler& func) {
+            handlers.emplace_back(func);
         }
 
         void Invoke(Args... args) {
-            for(const auto& func : listeners) {
+            for(const auto& func : handlers) {
                 func(args...);
             }
         }
     private:
         // functions that get called on invoke
-        std::vector<std::function<void(Args...)>> listeners{};
+        std::vector<Handler> handlers{};
         
     };
 }
